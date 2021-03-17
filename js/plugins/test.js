0be378430570e62e9c37f7eea2963987f29f8305
TKLNinja <tobias.weller02@gmail.com>
2021-03-17 18:55:09 +0100

    Update 0.3.0

--- js/plugins/test.js
+++
@@ -1,6 +0,0 @@
-faceId = Math.randomInt(4);
-$gameMessage.setFaceImage('ry/Imminent Tickling/1,4,5',faceId)
-$gameMessage.setBackground(0)
-$gameMessage.setPositionType(0)
-$gameMessage.add("Y\\.-You cur!\\.\\. Don't touch my feet!")
-this.setWaitMode('message')
